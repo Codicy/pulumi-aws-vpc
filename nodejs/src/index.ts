cc47032c51c87c602cbb2dd52496e373007af3d8
Graham Daley <graham@daleybread.com>
2021-10-29 22:00:17 +0800

    Optionally create NAT Instances instead of NAT Gateways

--- nodejs/src/index.ts
+++ nodejs/src/index.ts
@@ -7,8 +7,9 @@
  */
 
 import * as aws from "@pulumi/aws";
-import {ComponentResource, ComponentResourceOptions, Input, Output} from "@pulumi/pulumi";
-import {SubnetDistributor} from "./subnetDistributor";
+import * as pulumi from "@pulumi/pulumi";
+import { ComponentResource, ComponentResourceOptions, Input, Output } from "@pulumi/pulumi";
+import { SubnetDistributor } from "./subnetDistributor";
 
 export interface VpcArgs {
     description: string;
@@ -17,6 +18,8 @@ export interface VpcArgs {
     baseCidr: string;
     availabilityZoneNames: string[];
     zoneName?: string;
+
+    natInstancesInsteadOfGateways: boolean;
 
     endpoints: {
         s3: boolean;
@@ -34,6 +37,8 @@ export class Vpc extends ComponentResource {
     publicRouteTable: aws.ec2.RouteTable;
     privateRouteTables: aws.ec2.RouteTable[] = [];
     natGateways: aws.ec2.NatGateway[] = [];
+    natInstances: aws.ec2.Instance[] = [];
+    natInstanceEipAssociations: aws.ec2.EipAssociation[] = [];
     natElasticIpAddresses: aws.ec2.Eip[] = [];
 
     flowLogsGroup: aws.cloudwatch.LogGroup;
@@ -44,7 +49,7 @@ export class Vpc extends ComponentResource {
     private readonly baseTags: { [k: string]: Input<string> };
 
     constructor(name: string, args: VpcArgs, opts?: ComponentResourceOptions) {
-        super("jen20:aws-vpc", name, {}, opts);
+        super("codicy:aws-vpc", name, {}, opts);
 
         // Make base info available to other methods.
         this.name = name;
@@ -59,7 +64,7 @@ export class Vpc extends ComponentResource {
             tags: this.resourceTags({
                 Name: `${args.description} VPC`,
             }),
-        }, {parent: this});
+        }, { parent: this });
 
         // Internet Gateway
         this.internetGateway = new aws.ec2.InternetGateway(`${name}-igw`, {
@@ -67,7 +72,7 @@ export class Vpc extends ComponentResource {
             tags: this.resourceTags({
                 Name: `${args.description} VPC Internet Gateway`,
             }),
-        }, {parent: this.vpc});
+        }, { parent: this.vpc });
 
         // Private Hosted Zone and DHCP Options
         if (args.zoneName) {
@@ -77,7 +82,7 @@ export class Vpc extends ComponentResource {
                 }],
                 name: args.zoneName,
                 comment: `Private zone for ${args.zoneName}. Managed by Pulumi.`,
-            }, {parent: this});
+            }, { parent: this });
 
             this.dhcpOptionSet = new aws.ec2.VpcDhcpOptions(`${name}-dhcp-options`, {
                 domainName: this.privateZone.name,
@@ -85,12 +90,12 @@ export class Vpc extends ComponentResource {
                 tags: this.resourceTags({
                     Name: `${args.description} DHCP Options`,
                 }),
-            }, {parent: this.vpc});
+            }, { parent: this.vpc });
 
             new aws.ec2.VpcDhcpOptionsAssociation(`${name}-dhcp-options-assoc`, {
                 vpcId: this.vpc.id,
                 dhcpOptionsId: this.dhcpOptionSet.id,
-            }, {parent: this.dhcpOptionSet});
+            }, { parent: this.dhcpOptionSet });
         }
 
         // Calculate subnet address spaces and create subnets
@@ -105,7 +110,7 @@ export class Vpc extends ComponentResource {
                     tags: this.resourceTags({
                         Name: `${args.description} Public ${index + 1}`,
                     }),
-                }, {parent: this.vpc});
+                }, { parent: this.vpc });
             });
             this.privateSubnets = distributor.privateSubnets().map((cidr, index) => {
                 return new aws.ec2.Subnet(`${name}-private-${index + 1}`, {
@@ -115,7 +120,7 @@ export class Vpc extends ComponentResource {
                     tags: this.resourceTags({
                         Name: `${args.description} Private ${index + 1}`,
                     }),
-                }, {parent: this.vpc});
+                }, { parent: this.vpc });
             });
         }
 
@@ -126,19 +131,19 @@ export class Vpc extends ComponentResource {
                 tags: this.resourceTags({
                     Name: `${args.description} Public Route Table`,
                 }),
-            }, {parent: this.vpc});
+            }, { parent: this.vpc });
 
             new aws.ec2.Route(`${name}-route-public-sn-to-ig`, {
                 routeTableId: this.publicRouteTable.id,
                 destinationCidrBlock: "0.0.0.0/0",
                 gatewayId: this.internetGateway.id,
-            }, {parent: this.publicRouteTable});
+            }, { parent: this.publicRouteTable });
 
             this.publicSubnets.map((subnet, index) => {
                 return new aws.ec2.RouteTableAssociation(`${name}-public-rta-${index + 1}`, {
                     subnetId: subnet.id,
                     routeTableId: this.publicRouteTable.id,
-                }, {parent: this.publicRouteTable});
+                }, { parent: this.publicRouteTable });
             });
         }
 
@@ -151,33 +156,74 @@ export class Vpc extends ComponentResource {
                 tags: this.resourceTags({
                     Name: `${args.description} NAT Gateway EIP ${index + 1}`,
                 }),
-            }, {parent: privateSubnet}));
+            }, { parent: privateSubnet }));
 
-            this.natGateways.push(new aws.ec2.NatGateway(`${name}-nat-gateway-${index + 1}`, {
-                allocationId: this.natElasticIpAddresses[index].id,
-                subnetId: publicSubnet.id,
-                tags: this.resourceTags({
-                    Name: `${args.description} NAT Gateway ${index + 1}`,
-                }),
-            }, {parent: privateSubnet}));
+            if (args.natInstancesInsteadOfGateways) {
+                const ami = pulumi.output(aws.ec2.getAmi({
+                    filters: [{
+                        name: "name",
+                        values: ["amzn-ami-vpc-nat-*"],
+                    }],
+                    owners: ["137112412989"], // This owner ID is Amazon
+                    mostRecent: true,
+                }));
+                this.natInstances.push(new aws.ec2.Instance(`${name}-nat-instance-${index + 1}`, {
+                    instanceType: "t2.micro",
+                    ami: ami.id,
+                    subnetId: publicSubnet.id,
+                    sourceDestCheck: false,
+                    userData: `#!/bin/bash
+                               yum -y update
+                               yum install -y aws-cfn-bootstrap
+                               echo 1 > /proc/sys/net/ipv4/ip_forward
+                               echo 0 > /proc/sys/net/ipv4/conf/eth0/send_redirects
+                               /sbin/iptables -t nat -A POSTROUTING -o eth0 -s ${publicSubnet.cidrBlock} -j MASQUERADE
+                               /sbin/iptables-save > /etc/sysconfig/iptables
+                               mkdir -p /etc/sysctl.d/
+                               cat << NatConfFileMarker > /etc/sysctl.d/nat.conf
+                               net.ipv4.ip_forward = 1
+                               net.ipv4.conf.eth0.send_redirects = 0
+                               NatConfFileMarker`
+                }));
+                this.natInstanceEipAssociations.push(new aws.ec2.EipAssociation(`${name}-nat-instance-${index + 1}-eipAssoc`, {
+                    instanceId: this.natInstances[index].id,
+                    allocationId: this.natElasticIpAddresses[index].id
+                }));
+            } else {
+                this.natGateways.push(new aws.ec2.NatGateway(`${name}-nat-gateway-${index + 1}`, {
+                    allocationId: this.natElasticIpAddresses[index].id,
+                    subnetId: publicSubnet.id,
+                    tags: this.resourceTags({
+                        Name: `${args.description} NAT Gateway ${index + 1}`,
+                    }),
+                }, { parent: privateSubnet }));
+            }
 
             this.privateRouteTables.push(new aws.ec2.RouteTable(`${name}-private-rt-${index + 1}`, {
                 vpcId: this.vpc.id,
                 tags: this.resourceTags({
                     Name: `${args.description} Private Subnet RT ${index + 1}`,
                 }),
-            }, {parent: privateSubnet}));
+            }, { parent: privateSubnet }));
 
-            new aws.ec2.Route(`${name}-route-private-sn-to-nat-${index + 1}`, {
-                routeTableId: this.privateRouteTables[index].id,
-                destinationCidrBlock: "0.0.0.0/0",
-                natGatewayId: this.natGateways[index].id,
-            }, {parent: this.privateRouteTables[index]});
+            if (args.natInstancesInsteadOfGateways) {
+                new aws.ec2.Route(`${name}-route-private-sn-to-nat-${index + 1}`, {
+                    routeTableId: this.privateRouteTables[index].id,
+                    destinationCidrBlock: "0.0.0.0/0",
+                    instanceId: this.natInstances[index].id,
+                }, { parent: this.privateRouteTables[index] });
+            } else {
+                new aws.ec2.Route(`${name}-route-private-sn-to-nat-${index + 1}`, {
+                    routeTableId: this.privateRouteTables[index].id,
+                    destinationCidrBlock: "0.0.0.0/0",
+                    natGatewayId: this.natGateways[index].id,
+                }, { parent: this.privateRouteTables[index] });
+            }
 
             new aws.ec2.RouteTableAssociation(`${name}-private-rta-${index + 1}`, {
                 subnetId: privateSubnet.id,
                 routeTableId: this.privateRouteTables[index].id,
-            }, {parent: this.privateRouteTables[index]});
+            }, { parent: this.privateRouteTables[index] });
         }
 
         // Create gateway endpoints if necessary
@@ -186,7 +232,7 @@ export class Vpc extends ComponentResource {
                 vpcId: this.vpc.id,
                 serviceName: `com.amazonaws.${aws.config.region}.s3`,
                 routeTableIds: [this.publicRouteTable.id, ...this.privateRouteTables.map(x => x.id)],
-            }, {parent: this.vpc});
+            }, { parent: this.vpc });
         }
 
         if (args.endpoints.dynamodb) {
@@ -194,7 +240,7 @@ export class Vpc extends ComponentResource {
                 vpcId: this.vpc.id,
                 serviceName: `com.amazonaws.${aws.config.region}.dynamodb`,
                 routeTableIds: [this.publicRouteTable.id, ...this.privateRouteTables.map(x => x.id)],
-            }, {parent: this.vpc});
+            }, { parent: this.vpc });
         }
 
         this.registerOutputs({});
@@ -204,13 +250,13 @@ export class Vpc extends ComponentResource {
         this.flowLogsRole = new aws.iam.Role(`${this.name}-flow-logs-role`, {
             description: `${this.description} VPC Flow Logs`,
             assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal(aws.iam.Principals.VpcFlowLogsPrincipal),
-        }, {parent: this.vpc});
+        }, { parent: this.vpc });
 
         this.flowLogsGroup = new aws.cloudwatch.LogGroup(`${this.name}-vpc-flow-logs`, {
             tags: this.resourceTags({
                 Name: `${this.description} VPC Flow Logs`,
             }),
-        }, {parent: this.flowLogsRole});
+        }, { parent: this.flowLogsRole });
 
         new aws.iam.RolePolicy(`${this.name}-flow-log-policy`, {
             name: "vpc-flow-logs",
@@ -231,14 +277,14 @@ export class Vpc extends ComponentResource {
                     },
                 ],
             },
-        }, {parent: this.flowLogsRole});
+        }, { parent: this.flowLogsRole });
 
         new aws.ec2.FlowLog(`${this.name}-flow-logs`, {
             logDestination: this.flowLogsGroup.arn,
             iamRoleArn: this.flowLogsRole.arn,
             vpcId: this.vpc.id,
             trafficType: trafficType,
-        }, {parent: this.flowLogsRole});
+        }, { parent: this.flowLogsRole });
     }
 
     public privateSubnetIds(): Output<string>[] {
